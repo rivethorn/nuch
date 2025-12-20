2c3ff47ed7b7021e1eec02db5d5837240882cdaf
Hassan Qasemi <hassanak813@outlook.com>
2025-12-21 01:37:58 +0330

    add statusbar placeholder, add prompt for q

--- src/tui.rs
+++ src/tui.rs
@@ -1,9 +1,11 @@
 use std::path::PathBuf;
 
 use cursive::{
-    View,
+    Rect, Vec2, View,
     view::{Resizable, Scrollable},
-    views::{Dialog, DummyView, LinearLayout, SelectView},
+    views::{
+        Dialog, DummyView, FixedLayout, Layer, LinearLayout, OnLayoutView, SelectView, TextView,
+    },
 };
 use cursive_hjkl::HjklToDirectionWrapperView;
 
@@ -49,10 +51,34 @@ pub fn run_tui(blogs: Vec<PathBuf>, content: Vec<PathBuf>) {
                 .min_height(10),
         )
         .title("NUxt Content Handler")
-        .button("Quit", |s| s.quit()), // .full_screen(),
+        .button("Quit", |s| s.quit()),
     ));
 
-    siv.add_global_callback('q', |s| s.quit());
+    siv.add_global_callback('q', |s| {
+        s.add_layer(
+            Dialog::text("Do you want to quit?")
+                .button("Yes", |s| s.quit())
+                .button("No", |s| {
+                    s.pop_layer();
+                }),
+        );
+    });
+
+    let status = LinearLayout::horizontal().child(TextView::new("vim"));
+
+    siv.screen_mut().add_transparent_layer(
+        OnLayoutView::new(
+            FixedLayout::new().child(
+                Rect::from_point(Vec2::zero()),
+                Layer::new(status).full_width(),
+            ),
+            |layout, size| {
+                layout.set_child_position(0, Rect::from_size((0, size.y - 1), (size.x, 1)));
+                layout.layout(size);
+            },
+        )
+        .full_screen(),
+    );
 
     siv.run();
 }
