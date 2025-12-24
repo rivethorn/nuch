dc24beb34f8481c43d5f155b0418d29d43c37194
Hassan Qasemi <hassanak813@outlook.com>
2025-12-25 02:06:28 +0330

    add listing collections function

--- src/ui.rs
+++ src/ui.rs
@@ -1,16 +1,21 @@
 use anyhow::Result;
 use inquire::Select;
-use std::path::PathBuf;
+use std::{
+    fs::read_dir,
+    path::{Path, PathBuf},
+};
 
-pub fn list_blogs(dir: &std::path::Path, exclude_dir: Option<&std::path::Path>) -> Result<Option<PathBuf>> {
+use crate::config::CollectionPaths;
+
+pub fn list_blogs(dir: &Path, exclude_dir: Option<&CollectionPaths>) -> Result<Option<PathBuf>> {
     let mut markdown_files: Vec<_> = Vec::new();
     if dir.is_dir() {
-        for entry in std::fs::read_dir(&dir)? {
+        for entry in read_dir(&dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("md") {
                 if let Some(ex) = exclude_dir {
-                    let dest = ex.join(path.file_name().unwrap());
+                    let dest = ex.files.join(path.file_name().unwrap());
                     if dest.exists() {
                         continue;
                     }
@@ -34,11 +39,13 @@ pub fn list_blogs(dir: &std::path::Path, exclude_dir: Option<&std::path::Path>) 
                 .to_string()
         })
         .collect();
+
     let selection = Select::new("Select a Markdown file:", names)
         .with_vim_mode(true)
         .without_filtering()
         .with_help_message("hjkl to move, enter, esc to quit")
         .prompt_skippable()?;
+
     let selected_name = match selection {
         Some(name) => name,
         None => {
@@ -58,4 +65,34 @@ pub fn list_blogs(dir: &std::path::Path, exclude_dir: Option<&std::path::Path>) 
         .expect("Selected file should exist");
 
     Ok(Some(markdown_files[selected_index].clone()))
+}
+
+pub fn list_colletctions(cols: Vec<CollectionPaths>) -> Result<Option<CollectionPaths>> {
+    // if there is only one collection, select it automatically
+    if cols.len() == 1 {
+        return Ok(Some(cols[0].clone()));
+    }
+
+    let collection_names: Vec<_> = cols.iter().map(|c| c.name.clone()).collect();
+
+    let selection = Select::new("First, select your collection:", collection_names)
+        .with_vim_mode(true)
+        .without_filtering()
+        .with_help_message("hjkl to move, enter, esc to quit")
+        .prompt()?;
+
+    // let selected_name = match selection {
+    //     Some(name) => name,
+    //     None => {
+    //         println!("Cancelled.");
+    //         return Ok(None);
+    //     }
+    // };
+
+    let selected_index = cols
+        .iter()
+        .position(|c| c.name == selection)
+        .expect("Selected collection should exist");
+
+    Ok(Some(cols[selected_index].clone()))
 }
